ff85c044c71a45ba2be2cc5f6d969f780f64c3a1
sharifulb07 <sharifulb07@gmail.com>
2025-05-09 16:44:21 +0600

    useReducer hook and payload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 const initialState = {count:0};
 const reducer = (state, action) => {
   switch (action.type) {
@@ -8,6 +8,10 @@ const reducer = (state, action) => {
       return {count: state.count + 1};
     case 'decrement':
       return {count: state.count - 1};
+    case 'incrementByAmount':
+      return {count: state.count + action.payload};
+    case 'decrementByAmount':
+      return {count: state.count - action.payload};
       case 'reset':
         return {count:0 };
     default:
@@ -17,14 +21,27 @@ const reducer = (state, action) => {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleIncrement = () => {
+    dispatch({type: 'incrementByAmount', payload: inputValue});
+    setInputValue(0);
+  };
+  const handleDecrement = () => {
+    dispatch({type: 'decrementByAmount', payload: inputValue}); 
+    setInputValue(0);
+  };
 
 
   return (
     <div>
+      <input type="number" value={inputValue} onChange={(e) => setInputValue(Number(e.target.value))} />
       <p>Count: {state.count}</p>
       <button onClick={()=>dispatch({type:"increment"})}>Increment</button>
       <button onClick={()=>dispatch({type:"decrement"})}>decrement</button>
       <button onClick={()=>dispatch({type:"reset"})}>Reset</button>
+      <button onClick={handleIncrement}>Increment by {inputValue}</button>
+      <button onClick={handleDecrement}>Decrement by {inputValue}</button>  
     </div>
 
 
